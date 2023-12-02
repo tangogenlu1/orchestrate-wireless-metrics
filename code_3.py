3de7481c3e2a09aa8a3e30c9b6d2428a31e457a2
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-02 02:37:10 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,56 @@
+function main() {
+
+    data.forEach(item => {
+    });
+}
+    }
+
+    const data = [];
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+}
+function generateRandomData() {
+main();
+    return data;
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);if __name__ == "__main__":
+import random
+    data = generate_random_data()
+    main()
+def main():
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+    for item in data:
+    return data
+
+import random
+    data = generate_random_data()
+if __name__ == "__main__":
+def main():
+    return data
+
+
+def generate_random_data():
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+        print(f"Random Number: {item}")
+    for item in data:
+import random
+    data = generate_random_data()
+
+    return data
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+def main():
