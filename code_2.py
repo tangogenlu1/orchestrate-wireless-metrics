98c555099af352fc1d8a85344e7b81a1ab09aa85
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-04 20:07:16 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,52 +1,46 @@
-public class RandomDataGenerator {
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
 
+
+
+main();
+function main() {
+    data.forEach(item => {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
 }
-            System.out.println("Random Number: " + item);
-
-        for (int i = 0; i < 10; i++) {
-        int[] data = new int[10];
+    return data;
+}
+        console.log("Random Number: " + item);
+    });
+function generateRandomData() {
     }
-        }
-        }
-    public static void main(String[] args) {
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-    data = generate_random_data()
-
-
-    for item in data:
-if __name__ == "__main__":
-def main():
-import random
     return data
         print(f"Random Number: {item}")
-def generate_random_data():    for item in data:
-        print(f"Random Number: {item}")
-import random
-def main():
-
-if __name__ == "__main__":
 
 
-def generate_random_data():
     main()
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    data = generate_random_data()import random
 def generate_random_data():
-    for item in data:
-    return data
-def main():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
 
     data = generate_random_data()
-
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    main()
+    for item in data:
+def main():    return data;
+function main() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+        console.log("Random Number: " + item);
+
+    }
+
+    const data = generateRandomData();
+function generateRandomData() {
+    data.forEach(item => {
+
+    });
+}
+main();
