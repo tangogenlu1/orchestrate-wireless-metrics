4b1b10f1b4fc61ca34d688ef0706825e2b9822a6
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-04 10:02:44 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,55 +1,56 @@
-if __name__ == "__main__":
+
+
+
+
+import random
 def generate_random_data():
+if __name__ == "__main__":
+    main()
     data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+        print(f"Random Number: {item}")
+def main():
+    for item in data:def generate_random_data():
+
+
+import random
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+    return data
+
+    for item in data:if __name__ == "__main__":
     for item in data:
 
 
-
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-        print(f"Random Number: {item}")
-import random
-    return data
-def main():
 def generate_random_data():
-
-    return data
-import random
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():
     main()
-if __name__ == "__main__":
+import random
 
 
-        print(f"Random Number: {item}")
-    for item in data:
     data = generate_random_data()
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-        }
-    }
-        for (int item : data) {
-        int[] data = new int[10];
-
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-
-        }
-}function generateRandomData() {
-}
-    });
+        print(f"Random Number: {item}")}
     const data = generateRandomData();
-    return data;
+}
+function generateRandomData() {
 function main() {
+    for (let i = 0; i < 10; i++) {
 
-    }
+
+        console.log("Random Number: " + item);
 
         data.push(Math.floor(Math.random() * 100) + 1);
-    const data = [];
-        console.log("Random Number: " + item);
-}
-
-    data.forEach(item => {
 main();
-    for (let i = 0; i < 10; i++) {
+    return data;
+    data.forEach(item => {
+    }
+    const data = [];
+    });
