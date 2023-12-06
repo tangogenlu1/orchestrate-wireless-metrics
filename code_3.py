f2e7b5a3b76c4bbc50c3235c4823479e6b7cb4cb
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-06 12:25:08 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,59 +1,41 @@
-    const data = generateRandomData();
-}
 
     for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
     const data = [];
 }
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-function generateRandomData() {
 
-    }
 
-        console.log("Random Number: " + item);
     return data;
-    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
 main();
+    data.forEach(item => {
+    });
+function generateRandomData() {
+    }
+        console.log("Random Number: " + item);
+function main() {import random
+
+    data = generate_random_data()
+
+    return data
         print(f"Random Number: {item}")
-    for item in data:
-
-
-def main():
 def generate_random_data():
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-    main()
-import random
-    data = generate_random_data()
-    return data
-
-    data = generate_random_data()
-def main():
-
-    main()
+    for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-if __name__ == "__main__":
-    for item in data:
-        print(f"Random Number: {item}")
-import random
-def generate_random_data():
-main();
-    });
-    const data = generateRandomData();
-    const data = [];
-
+    main()
+def main():
+if __name__ == "__main__":            System.out.println("Random Number: " + item);
 }
-function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        for (int item : data) {
+
+        for (int i = 0; i < 10; i++) {
     }
-function main() {
-    data.forEach(item => {
-
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
-}
-    return data;
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+        int[] data = new int[10];
