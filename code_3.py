57417829920d093cf930d13af319d34625075d1b
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-08 09:45:18 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,50 @@
-    main()
-import random
-    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    for item in data:
+
+def main():
+
+def generate_random_data():
+    return data
         print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+
+import random    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+def generate_random_data():
+    return data
+        print(f"Random Number: {item}")
+
+    main()
+def main():
 if __name__ == "__main__":
 
 
-    data = generate_random_data()
-    return data
-def generate_random_data():
-def main():
+    for item in data:
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+}
+    }
 
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    }
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
